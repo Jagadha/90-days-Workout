c0aed2878462261f0716bace0e2424161376ddfd
Jagadha Murali <63654340+Jagadha@users.noreply.github.com>
2024-07-31 11:34:59 +0100

    Update HelloWorld.java

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -2,7 +2,15 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Properties prop = new Properties();
+		prop.load(new FileInputStream("Config.properties"));
+		
 		System.out.println("Hello World!!");
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(prop.getProperty("url"));
+		driver.close();
 
 	}
 
